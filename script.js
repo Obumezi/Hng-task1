915e80387ced6f859ebb79512bfe7f076cabd0c9
Obumneme Eziamaka <eziamakaobum@yahoo.co.uk>
2023-09-10 16:43:07 +0100

    time fix

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ function updateDateTime() {
     const dayOfWeekElement = document.getElementById('dayOfWeek');
     const utcTimeElement = document.getElementById('utcTime');
     const currentDate = new Date();
-    const options = { weekday: 'long' };
 
-    const formattedDayOfWeek = currentDate.toLocaleDateString('en-US', options);
-    const currentUTCTime = currentDate.getTime();
+    // Format day of the week
+    const options = { weekday: 'long' };
+    const formattedDayOfWeek = new Intl.DateTimeFormat('en-US', options).format(currentDate);
+
+    // Format UTC time with milliseconds
+    const utcOptions = { timeZone: 'UTC', hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit', fractionalSecondDigits: 3 };
+    const formattedUTCTime = new Intl.DateTimeFormat('en-US', utcOptions).format(currentDate);
 
     dayOfWeekElement.textContent = formattedDayOfWeek;
-    utcTimeElement.textContent = currentUTCTime;
+    utcTimeElement.textContent = formattedUTCTime;
 }
 
-// Update the date and time every second
-setInterval(updateDateTime, 1000);
+// Update the date and time every millisecond (for milliseconds)
+setInterval(updateDateTime, 1);
 
 // Initial update
 updateDateTime();
